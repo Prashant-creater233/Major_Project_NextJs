be44ed57808db252d8e2ac50f9004335d780e027
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-09-20 22:36:13 +0530

    Major project

---
+++ models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+interface IUser{
+    _id?: mongoose.Types.ObjectId;
+    name: string;
+    email: string;
+    password: string;
+    mobile?: string;
+    role: "user" | "delieveryBoy" | "admin"
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        enum: ["user", "deleivery", "admin"],
+        default: "user"
+    }
+},
+{timestamps: true})
+
+const User = mongoose.models.User || mongoose.model("User", userSchema)
+export default User;
